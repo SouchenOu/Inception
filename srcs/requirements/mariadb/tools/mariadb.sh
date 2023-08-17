a97226fb274a4cce36b2cad2bd65060011100751
Soukaina Ouchen <souchen@e3r9p1.1337.ma>
2023-08-17 14:08:51 +0100

    build mariadb image

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,17 +1,15 @@
+#!/bin/bash
+service mysql start
+mysql  -u root -p -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
+mysql -u root -p -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
+mysql  -u root -p -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
+mysql  -u root -p -e "FLUSH PRIVILEGES;"
+exec mysqld_safe
 
-#Pour demarer mySql
-service mysql start;
 
-#create database table
-mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE"
 
-#After create database_table we will create a user who can manipulate it
-mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'localhost' IDENTIFIED BY '$SQL_PASSWORD';"
-
-#I give all rights to this user(Je donne tous les droits à cet utilisateur.)
-
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`*.* TO \`${SQL_USER}\`
-
+# /bin/sh is an executable representing the system shell and usually implemented as a symbolic link pointing to the executable for whichever shell is the system shell. The system shell is basically the default shell that the script should use
 
 
 
@@ -20,35 +18,5 @@ mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`*.* TO \`${SQL_USER}\`
 
 
 
-
-
-
-
-
-
-
-
-
-# systemctl status mariadb
-# mysql_secure_installation
-# mysql -u root -p
-# CREATE DATABASE IF NOT EXIST $DB_NAME;
-# CREATE USER $DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSW';
-# GRANT ALL PRIVILEGES ON *.* to '$DB_USER'@'localhost';
-# FLUSH PRIVILEGES;
-
-
-
-
-
-# service mysql start
-# mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
-# mysql -u root -p -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'" ;
-# mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" ;
-# mysql -u root -p -e "FLUSH PRIVILEGES;"
-# mysql -u root -p -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
-# mysql -u root -p -e "FLUSH PRIVILEGES;"
-# kill `cat /var/run/mysqld/mysqld.pid`
-# mysqld
 
 
