da5652bec5dfe9ef64095f8dad0cadcd8c3f48b9
Soukaina Ouchen <souchen@e3r9p1.1337.ma>
2023-08-30 15:27:49 +0100

    stop here

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 service mysql start
 mysql  -u root -p -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
-mysql -u root -p -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
+mysql -u root -p -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'localhost' IDENTIFIED BY '$SQL_PASSWORD'" ;
 mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
 mysql  -u root -p -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
 mysql  -u root -p -e "FLUSH PRIVILEGES;"
@@ -11,10 +11,12 @@ exec mysqld_safe
 
 # /bin/sh is an executable representing the system shell and usually implemented as a symbolic link pointing to the executable for whichever shell is the system shell. The system shell is basically the default shell that the script should use
 
+#Do note that there is no need to use this mechanism to create the root superuser, that user gets created by default with the password specified by the MYSQL_ROOT_PASSWORD variable.
 
 
+# If you want to give the privileges of all the databases and tables to a user. You can run the following command using the “*.*”
 
-
+#example on privileges ---> insert, select, delete ... 
 
 
 
