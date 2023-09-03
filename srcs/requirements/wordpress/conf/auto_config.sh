ec0dbbf3ee75e2b53852649d230b5b8e14734367
Soukaina Ouchen <souchen@e3r9p1.1337.ma>
2023-09-03 18:27:41 +0100

    try to connect wordpress with database

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
+touch /var/www/wordpress/wp-config.php;
+sed -i "s/votre_nom_de_bdd/${SQL_DATABASE}/g" "/var/www/wordpress/wp-config-sample.php"
+sed -i "s/votre_utilisateur_de_dbb/${SQL_USER}/g" "/var/www/wordpress/wp-config-sample.php"
+sed -i "s/votre_mdp_de_bdd/${SQL_PASSWORD}/g" "/var/www/wordpress/wp-config-sample.php"
+sed -i "s/localhost/${db_host}/g" "/var/www/wordpress/wp-config-sample.php"
+cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php;
+exec "$@"
