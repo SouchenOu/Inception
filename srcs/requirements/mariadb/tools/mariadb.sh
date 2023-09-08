828ece3999355cfee29bc85e56445eea658cb99b
Soukaina Ouchen <souchen@e3r9p1.1337.ma>
2023-09-08 12:08:03 +0100

    stop here today

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,43 +1,12 @@
-#!/bin/bash
-
-# starting the mysql service
+##!/bin/bash
 service mysql start
-
-# change the bind to 0.0.0.0 only accept client connections made to 0.0.0.0 (accept connection to any address)
-# sed -i 's/bind-address= 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# create the database if not exist
-mysql -u root -p $MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
-
-# create the user if not exist
-mysql -u root -p $MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
-
-# grant all priviliges on the created database to the user
-mysql -u root -p $MYSQL_ROOT_PASSWORD-e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';"
-
-# this command tell the MySQL or MariaDB server to reload the grant tables and update its internal data structures with the current contents of the grant tables.
-mysql -u root -p $MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-
-# set the password to the root
-mysql -u root -p $MYSQL_ROOT_PASSWORD-e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-
-# killing the porcess of mysqld to not restarting while waiting the wordpress to get setup
+mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
+mysql -u root -p -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
+mysql -u root -p -e "FLUSH PRIVILEGES;"
+mysql -u root -p -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
+mysql -u root -p -e "FLUSH PRIVILEGES;"
 kill `cat /var/run/mysqld/mysqld.pid`
-
-exec "$@"
-
-#FLUSH PRIVILEGES ---> Plus qu’à rafraichir tout cela pour que MySQL le prenne en compte.
-
-# /bin/sh is an executable representing the system shell and usually implemented as a symbolic link pointing to the executable for whichever shell is the system shell. The system shell is basically the default shell that the script should use
-
-#Do note that there is no need to use this mechanism to create the root superuser, that user gets created by default with the password specified by the MYSQL_ROOT_PASSWORD variable.
-
-
-# If you want to give the privileges of all the databases and tables to a user. You can run the following command using the “*.*”
-
-#example on privileges ---> insert, select, delete ... 
-
-
-
+mysqld
 
 
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+	mkdir -p /run/php/;
+	touch /run/php/php7.3-fpm.pid;
+	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 	apt-get -y install wget
  	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar; 
 	mv wp-cli.phar /usr/local/bin/wp; 
-	cd /var/www/html;
-	chmod -R 755 /var/www/html
-	# download wordpress using wp-CLI 
-	wp core download --allow-root
+	wget https://wordpress.org/latest.tar.gz -P /var/www
+	cd /var/www
+	tar -xzvf latest.tar.gz
+	cd /var/www/wordpress
+	chown -R root:root /var/www/wordpress
 	touch wp-config.php
-	# wget https://wordpress.org/latest.tar.gz -P /var/www
-    # cd /var/www && tar -xzvf latest.tar.gz && rm latest.tar.gz
-	sed -i "s/database_name_here/${SQL_DATABASE}/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/username_here/${SQL_USER}/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/password_here/${SQL_PASSWORD}/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/localhost/${db_host}/g" "/var/www/html/wp-config-sample.php"
-	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+	sed -i "s/database_name_here/${SQL_DATABASE}/g" "/var/www/wordpress/wp-config-sample.php"
+	sed -i "s/username_here/${SQL_USER}/g" "/var/www/wordpress/wp-config-sample.php"
+	sed -i "s/password_here/${SQL_PASSWORD}/g" "/var/www/wordpress/wp-config-sample.php"
+	sed -i "s/localhost/${db_host}/g" "/var/www/wordpress/wp-config-sample.php"
+	cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 	
 exec "$@"
