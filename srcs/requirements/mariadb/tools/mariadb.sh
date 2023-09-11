11b77fd4df6525a78c13a594ec9156db3c7d1c35
Soukaina Ouchen <souchen@e3r9p1.1337.ma>
2023-09-11 13:32:32 +0100

    fexing mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,12 +1,12 @@
 ##!/bin/bash
 service mysql start
-mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
-mysql -u root -p -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -u root -p -e "FLUSH PRIVILEGES;"
-mysql -u root -p -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 mysql -u root -p -e "FLUSH PRIVILEGES;"
-kill `cat /var/run/mysqld/mysqld.pid`
-mysqld
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+exec mysqld_safe
 
 
