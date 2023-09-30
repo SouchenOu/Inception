13f87de2acb7125c3b8d405688c4e3385a6e0638
Soukaina Ouchen <souchen@e3r8p1.1337.ma>
2023-09-30 15:03:16 +0100

    a wont to add networking and fix the problem i have in database

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
-
-MYSQL_ROOT_PASSWORD=123456789
-USER_EMAIL=souchen@student.1337.ma
-SQL_DATABASE=wordp_db
-SQL_USER=souchen
-SQL_PASSWORD=123456789
-db_host=localhost
-
 service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
-mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
+mysql -u root "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
+mysql -u root "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
+mysql -u root "FLUSH PRIVILEGES;"
+mysql -u root "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
+mysql -u root "FLUSH PRIVILEGES;"
 # kill `cat /var/run/mysqld/mysqld.pid`
 mysqld
+
+
+
+
+
+
+
+
+
+
+
+
+# service mysql start
+# mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
+# mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
+# mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
+# mysql -e "FLUSH PRIVILEGES;"
+# mysql -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
+# mysql -e "FLUSH PRIVILEGES;"
+# # kill `cat /var/run/mysqld/mysqld.pid`
+# mysqld
 
 
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -3,22 +3,20 @@
 	touch /run/php/php7.3-fpm.pid;
 	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 	apt-get -y install wget
-	mkdir -p /var/www/html
+	wget https://fr.wordpress.org/wordpress-6.3.1-en_GB.tar.gz -P /var/www
+	cd /var/www 
+	tar -xzf wordpress-6.3.1-en_GB.tar.gz 
+	rm wordpress-6.3.1-en_GB.tar.gz
+	chmod -x /var/www/wordpress
  	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar; 
 	mv wp-cli.phar /usr/local/bin/wp; 
-	cd /var/www/html;
-	# wget https://wordpress.org/latest.tar.gz -P /var/www
-	wp core download --allow-root;
-	# cd /var/www
-	# tar -xzvf latest.tar.gz
-	# cd /var/www/wordpress
-	# chown -R root:root /var/www/wordpress
-	touch /var/www/html/wp-config.php
-	sed -i "s/database_name_here/${SQL_DATABASE}/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/username_here/${SQL_USER}/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/password_here/${SQL_PASSWORD}/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/localhost/${db_host}/g" "/var/www/html/wp-config-sample.php"
-	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-	
+	cd /var/www/wordpress
+	touch /var/www/wordpress/wp-config.php
+	sed -i "s/database_name_here/${SQL_DATABASE}/g" "/var/www/wordpress/wp-config-sample.php"
+	sed -i "s/username_here/${SQL_USER}/g" "/var/www/wordpress/wp-config-sample.php"
+	sed -i "s/password_here/${SQL_PASSWORD}/g" "/var/www/wordpress/wp-config-sample.php"
+	sed -i "s/localhost/${db_host}/g" "/var/www/wordpress/wp-config-sample.php"
+	cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+	echo "success!"
 exec "$@"
