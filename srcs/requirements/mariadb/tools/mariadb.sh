6399c00c3a3726ad931a62a5e2174281afab3710
Soukaina Ouchen <souchen@e3r8p1.1337.ma>
2023-10-01 14:33:33 +0100

    serve wordpress website succeffuly

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
 service mysql start
-mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
-mysql -u root -p -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
-mysql -u root -p -e "FLUSH PRIVILEGES;"
-mysql -u root -p -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
-mysql -u root -p -e "FLUSH PRIVILEGES;"
+mysql -u root -password -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
+mysql -u root -password -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
+mysql -u root -password -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
+mysql -u root -password -e "FLUSH PRIVILEGES;"
+mysql -u root -password -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
+mysql -u root -password -e "FLUSH PRIVILEGES;"
 kill `cat /var/run/mysqld/mysqld.pid`
 mysqld
-
-
-
-
-
-
-
-
-
-
-
-
-# service mysql start
-# mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
-# mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
-# mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
-# mysql -e "FLUSH PRIVILEGES;"
-# mysql -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
-# mysql -e "FLUSH PRIVILEGES;"
-# # kill `cat /var/run/mysqld/mysqld.pid`
-# mysqld
-
 
