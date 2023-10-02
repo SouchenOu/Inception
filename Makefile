248d8838d5ce61b44824a7fe742d14a7dc53c807
Soukaina Ouchen <souchen@e3r8p1.1337.ma>
2023-10-02 17:32:53 +0100

    ~Added Docker convenience scripts

--- Makefile
+++ Makefile
@@ -4,10 +4,7 @@ build:
 	mkdir -p /Users/souchen/data/wp
 
 up:
-	docker-compose -f ./srcs/docker-compose.yml up
-
-down:
-	docker-compose -f ./srcs/docker-compose.yml down
+	docker-compose -f ./srcs/docker-compose.yml up -d
 
 restart:
 	make down
@@ -15,14 +12,16 @@ restart:
 	make build
 	make up
 
+down:
+	docker-compose -f ./srcs/docker-compose.yml down
 
 clean:
 	docker stop $$(docker ps -a);\
-	docker rm $$(docker ps -a);\
 	docker rmi -f $$(docker images -a);\
-	docker volume rm $$(docker volume ls);\
+	docker rm $$(docker ps -a);\
 	docker network rm $$(docker network ls);\
+	docker volume rm $$(docker volume ls);\
 	rm -rf /Users/souchen/data/db/\
 	rm -rf /Users/souchen/data/wp/\
 
-.PHONY: build up down restart clean	
+.PHONY: build up restart down clean	
--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 service mysql start
-mysql -u root -password -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
-mysql -u root -password -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
-mysql -u root -password -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
-mysql -u root -password -e "FLUSH PRIVILEGES;"
-mysql -u root -password -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
-mysql -u root -password -e "FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
+mysql -u root -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
+mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
+mysql -u root -e "FLUSH PRIVILEGES;"
 kill `cat /var/run/mysqld/mysqld.pid`
 mysqld
 
+doc
