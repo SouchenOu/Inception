3b591bdee3705233143abc3bfb1ef33c8665d2cc
SouchenOu <soukainaouchenuai@gmail.com>
2023-10-05 16:17:52 +0100

    finish inception

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -9,13 +9,6 @@ mysql -u root -p"$MYSQL_ROOT_PASSWORD"  -e  "CREATE USER IF NOT EXISTS '$SQL_USE
 mysql -u root -p"$MYSQL_ROOT_PASSWORD"  -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
 mysql -u root -p"$MYSQL_ROOT_PASSWORD"  -e  "FLUSH PRIVILEGES;"
 
-#mysql -u root -p"$MYSQL_ROOT_PASSWORD" << _EOF_
-#CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;
-# CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
-# GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';
-# FLUSH PRIVILEGES;
-# _EOF_
-
 #mysql -u root -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
 
 mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
