2c59ed3bbbc9393512fbd5e5d1dfad1d05546908
SouchenOu <soukainaouchenuai@gmail.com>
2023-10-05 18:34:20 +0100

    push

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 service mariadb start
-#/etc/init.d/mariadb start
 
+#password that will be set for the Mysql root superuser account
 mysqladmin -u root password "${MYSQL_ROOT_PASSWORD}"
 
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e  "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
@@ -9,8 +9,8 @@ mysql -u root -p"$MYSQL_ROOT_PASSWORD"  -e  "CREATE USER IF NOT EXISTS '$SQL_USE
 mysql -u root -p"$MYSQL_ROOT_PASSWORD"  -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
 mysql -u root -p"$MYSQL_ROOT_PASSWORD"  -e  "FLUSH PRIVILEGES;"
 
-#mysql -u root -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
-
+#redemarer MYSQL pour tout cela soit effectif
 mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 
+#Je commence deja par eteindre MYSQL (Turn off)
 exec mysqld
