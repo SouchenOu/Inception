85c7ae15e7e67c433821780dba3e00dc16d22af2
SouchenOu <soukainaouchenuai@gmail.com>
2023-10-06 09:22:22 +0100

    final fixing error

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,10 +4,11 @@ service mariadb start
 #password that will be set for the Mysql root superuser account
 mysqladmin -u root password "${MYSQL_ROOT_PASSWORD}"
 
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e  "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
-mysql -u root -p"$MYSQL_ROOT_PASSWORD"  -e  "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'" ;
-mysql -u root -p"$MYSQL_ROOT_PASSWORD"  -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';" ;
-mysql -u root -p"$MYSQL_ROOT_PASSWORD"  -e  "FLUSH PRIVILEGES;"
+mysql -u root  -p"$MYSQL_ROOT_PASSWORD" -e  "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE";
+mysql -u root  -p"$MYSQL_ROOT_PASSWORD" -e  "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+mysql -u root  -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';"
+mysql -u root  -e "ALTER USER  'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -u root  -p"$MYSQL_ROOT_PASSWORD" -e  "FLUSH PRIVILEGES;"
 
 #redemarer MYSQL pour tout cela soit effectif
 mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
